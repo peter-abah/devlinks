1c24cfa49d6434a4e24e0e7254b66fb5547caafe
Peter Abah <peterabah.ob@gmail.com>
2024-07-25 07:34:31 +0100

    fix: make profile form responsive

--- components/profile-form.tsx
+++ components/profile-form.tsx
@@ -148,101 +148,103 @@ export default function ProfileForm() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="bg-white grow rounded-xl stack max-h-full">
-      <div className="p-6 md:p-10 stack gap-6 grow max-h-full">
+      <div className="p-6 md:p-10 stack gap-10 grow max-h-full">
         <header className="space-y-2 shrink-0">
           <h1 className="text-2xl md:text-[32px] font-bold">Profile Details</h1>
           <p className="text-gray">Add your details to create a personal touch to your profile.</p>
         </header>
 
-        <div className="stack gap-6 text-gray mb-6">
-          <div className="w-full flex gap-4 items-center p-5 bg-gray-light justify-between">
-            <Label className="text-base shrink-0">Profile Picture</Label>
-            <div className="flex items-center gap-6">
-              <label
-                htmlFor="profile-picture"
-                className="w-[193px] relative overflow-hidden shrink-0 aspect-square rounded-xl text-primary hover:opacity-60 bg-primary-lightest grid place-items-center cursor-pointer"
-              >
-                {imgPreviewToUse && (
-                  <>
-                    <Image
-                      src={imgPreviewToUse.url}
-                      alt={imgPreviewToUse.name}
-                      fill
-                      className="object-cover"
-                    />
-                    <span className="bg-[#000] bg-opacity-50 w-full h-full top-0 left-0 absolute z-10"></span>
-                  </>
-                )}
-                <div
-                  className={cn("stack relative z-10 gap-2 items-center", {
-                    "text-white": !!imgPreviewToUse,
+        <div className="stack gap-6">
+          <div className="stack gap-6 text-gray mb-6">
+            <div className="w-full flex flex-col rounded-xl md:flex-row gap-4 md:items-center p-5 bg-gray-light justify-between">
+              <Label className="text-base shrink-0">Profile Picture</Label>
+              <div className="flex flex-col md:flex-row md:items-center gap-6">
+                <label
+                  htmlFor="profile-picture"
+                  className="w-[193px] relative overflow-hidden shrink-0 aspect-square rounded-xl text-primary hover:opacity-60 bg-primary-lightest grid place-items-center cursor-pointer"
+                >
+                  {imgPreviewToUse && (
+                    <>
+                      <Image
+                        src={imgPreviewToUse.url}
+                        alt={imgPreviewToUse.name}
+                        fill
+                        className="object-cover"
+                      />
+                      <span className="bg-[#000] bg-opacity-50 w-full h-full top-0 left-0 absolute z-10"></span>
+                    </>
+                  )}
+                  <div
+                    className={cn("stack relative z-10 gap-2 items-center", {
+                      "text-white": !!imgPreviewToUse,
+                    })}
+                  >
+                    <ImageIcon />
+                    <p className="font-semibold">
+                      {imgPreviewToUse ? "Change Image" : "+ Upload Image"}
+                    </p>
+                  </div>
+                </label>
+
+                <input
+                  type="file"
+                  id="profile-picture"
+                  className="sr-only"
+                  accept="image/*"
+                  {...register("profilePicture")}
+                />
+                <p
+                  className={cn("text-xs max-w-[215px] min-[1440px]:w-[215px]", {
+                    "text-destructive": !!errors.profilePicture,
                   })}
                 >
-                  <ImageIcon />
-                  <p className="font-semibold">
-                    {imgPreviewToUse ? "Change Image" : "+ Upload Image"}
-                  </p>
-                </div>
-              </label>
-
-              <input
-                type="file"
-                id="profile-picture"
-                className="sr-only"
-                accept="image/*"
-                {...register("profilePicture")}
-              />
-              <p
-                className={cn("text-xs max-w-[215px] min-[1440px]:w-[215px]", {
-                  "text-destructive": !!errors.profilePicture,
-                })}
-              >
-                {errors.profilePicture
-                  ? errors.profilePicture.message
-                  : "Image must be below 1024x1024px. Use PNG or JPG format."}
-              </p>
+                  {errors.profilePicture
+                    ? errors.profilePicture.message
+                    : "Image must be below 1024x1024px. Use PNG or JPG format."}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="rounded-lg bg-gray-light p-5 text-gray stack gap-3">
-          <div className="flex gap-4 items-center justify-between">
-            <Label htmlFor="firstName" className="text-base shrink-0">
-              First name*
-            </Label>
-            <InputWithIcon
-              id="firstName"
-              containerClassName="max-w-[432px] text-foreground"
-              placeholder="e.g. John"
-              {...register("first_name")}
-              errorMessage={errors.first_name && errors.first_name.message}
-            />
-          </div>
+          <div className="rounded-lg bg-gray-light p-5 text-gray stack gap-3">
+            <div className="flex flex-col gap-1 md:gap-4 md:flex-row md:items-center justify-between">
+              <Label htmlFor="email" className="md:text-base shrink-0">
+                First name*
+              </Label>
+              <InputWithIcon
+                id="firstName"
+                containerClassName="max-w-[432px] text-foreground"
+                placeholder="e.g. John"
+                {...register("first_name")}
+                errorMessage={errors.first_name && errors.first_name.message}
+              />
+            </div>
 
-          <div className="flex gap-4 items-center justify-between">
-            <Label htmlFor="lastName" className="text-base shrink-0">
-              Last name*
-            </Label>
-            <InputWithIcon
-              id="lastName"
-              containerClassName="max-w-[432px] text-foreground"
-              placeholder="e.g. Appleseed"
-              {...register("last_name")}
-              errorMessage={errors.last_name && errors.last_name.message}
-            />
-          </div>
+            <div className="flex flex-col gap-1 md:gap-4 md:flex-row md:items-center justify-between">
+              <Label htmlFor="email" className="md:text-base shrink-0">
+                Last name*
+              </Label>
+              <InputWithIcon
+                id="lastName"
+                containerClassName="max-w-[432px] text-foreground"
+                placeholder="e.g. Appleseed"
+                {...register("last_name")}
+                errorMessage={errors.last_name && errors.last_name.message}
+              />
+            </div>
 
-          <div className="flex gap-4 items-center justify-between">
-            <Label htmlFor="email" className="text-base shrink-0">
-              Email
-            </Label>
-            <InputWithIcon
-              id="email"
-              containerClassName="max-w-[432px] text-foreground"
-              placeholder="e.g. email@example.com"
-              {...register("email")}
-              errorMessage={errors.email && errors.email.message}
-            />
+            <div className="flex flex-col gap-1 md:gap-4 md:flex-row md:items-center justify-between">
+              <Label htmlFor="email" className="md:text-base shrink-0">
+                Email
+              </Label>
+              <InputWithIcon
+                id="email"
+                containerClassName="max-w-[432px] text-foreground"
+                placeholder="e.g. email@example.com"
+                {...register("email")}
+                errorMessage={errors.email && errors.email.message}
+              />
+            </div>
           </div>
         </div>
       </div>
